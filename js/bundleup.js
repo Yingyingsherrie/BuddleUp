08387f1db26078214d569cc7310ccd889a661055
Alex Ayerdi <AlexanderAyerdi2015@u.northwestern.edu>
2014-04-04 16:56:10 -0500

    Added images to fill in based on weather.

--- js/bundleup.js
+++ js/bundleup.js
@@ -25,8 +25,10 @@ $(function(){
 		success: function(parsed_json) {
 		// var location = parsed_json['location']['city'];
 		var p1 = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['pop'];
-
+		var p2 = parsed_json['forecast']['txt_forecast']['forecastday'][((time < 17) ? 0 : 1)]['icon_url'];
+		
 		$("#precip").append(p1);
+		$("#weather").prop("src", p2);
 		}
 	});
 });
